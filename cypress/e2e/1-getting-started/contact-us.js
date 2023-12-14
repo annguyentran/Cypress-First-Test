71930aadf7ec49d555524454cb1edc5801ebffa0
Andrew Nguyen-Tran <ann013@ucsd.edu>
2023-12-14 11:35:25 -0800

    Starting the cypress test

---
+++ cypress/e2e/1-getting-started/contact-us.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("Test contact us form via WebdriverUni", () => {
+    it("Should be able to submit a sucessful submission via contact us form", () => {
+        "Cypress code"
+        cy.click
+    })
+
+    it("Should not be able to submit a sucessful submission via contact us form as all fields are required", () => {
+        "Cypress code"
+    })
+});
