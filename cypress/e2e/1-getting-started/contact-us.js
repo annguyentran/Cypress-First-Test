1976636b13bcddd96c7ce94059bec9582b6ffb28
Andrew Nguyen-Tran <ann013@ucsd.edu>
2023-12-14 21:47:45 -0800

    Creating positive and negative test cases

--- cypress/e2e/1-getting-started/contact-us.js
+++ cypress/e2e/1-getting-started/contact-us.js
@@ -3,10 +3,20 @@
 describe("Test contact us form via WebdriverUni", () => {
     it("Should be able to submit a sucessful submission via contact us form", () => {
         "Cypress code"
-        cy.click
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        //cy.get('#contact-us').click();
+        cy.get('[name="first_name"]').type('Andrew');
+        cy.get('[name="last_name"]').type('N');
+        cy.get('[name="email"]').type('andrew.noreply@gmail.com');
+        cy.get('textarea.feedback-input').type('Text');
+        cy.get('[type="submit"]').click();
     })
 
-    it("Should not be able to submit a sucessful submission via contact us form as all fields are required", () => {
-        "Cypress code"
-    })
+    it.only("Should not be able to submit a sucessful submission via contact us form as all fields are required", () => {
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type('Andrew');
+        cy.get('[name="last_name"]').type('N');
+        ///cy.get('[name="email"]').type('andrew.noreply@gmail.com');
+        cy.get('textarea.feedback-input').type('Text');
+        cy.get('[type="submit"]').click();    })
 });
