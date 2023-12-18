d221a3b5dadd0636c6c3cbc9b8fac6cce49fd454
Andrew Nguyen-Tran <ann013@ucsd.edu>
2023-12-17 17:33:38 -0800

    Added xpath selectors and examples

---
+++ cypress/e2e/Automation-test-store/contact-us.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+
+describe("Test contact us form via automation test store", () => {
+    it("Should be able to submit a sucessful submission via contact us form", () => {
+        "Cypress code"
+        cy.visit('https://www.automationteststore.com/')
+        cy.xpath('//a[contains(@href, "contact")]').click();
+        cy.get('#ContactUsFrm_first_name').type("Andrew");
+        cy.get('#ContactUsFrm_email').type('andrew@gmail.com');
+        cy.get('#ContactUsFrm_enquiry').type('Testing');
+        cy.get("button[title='Submit']").click();
+    })
+
+   
+});
