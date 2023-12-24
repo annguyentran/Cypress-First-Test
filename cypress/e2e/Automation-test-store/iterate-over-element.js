e870e93c35daf944679d0f73f72de686ca5f58e2
Andrew Nguyen-Tran <ann013@ucsd.edu>
2023-12-23 19:03:15 -0800

    Iterating over an array of hair care products

--- cypress/e2e/Automation-test-store/iterate-over-element.js
+++ cypress/e2e/Automation-test-store/iterate-over-element.js
@@ -6,12 +6,30 @@ describe("Iterate over elements", () => {
         cy.visit("https://automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
 
-        cy.get('ul>li').each(($el, index, $list) => {
-            
+        // Get all of the elements of the hair care in the list and each item in the list will be iterated.
+
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+
+            // Log the index and the element text in the cypress logs over each iteration.
+
+            cy.log("Index: " + index + " :: " + $el.text());
+
         })
     });
     it("Add specific product to basket", () => {
         cy.visit("https://automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+
+        
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+
+        if($el.text().includes('Curls to straight Shampoo')) {
+            
+            // warps allows us to use cypress commands on elements like .click() instead of using jquery methods 
+
+            cy.warp($el).click();
+        }
+            
+        })
     });
 });
