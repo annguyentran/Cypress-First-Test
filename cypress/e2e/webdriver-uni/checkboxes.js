8da10a9c0b0b4bba4f2012a7e4d46d8f64faf542
Andrew Nguyen-Tran <ann013@ucsd.edu>
2024-01-06 22:09:43 -0800

    working with checkboxes

---
+++ cypress/e2e/webdriver-uni/checkboxes.js
@@ -0,0 +1,43 @@
+
+describe("Verify checkboxes via webdriveruni", () => {
+    it("Check and validate checkbox", () => {
+        cy.visit("https://www.webdriveruniversity.com")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+
+        // These are ways we can check the boxes and validate them 
+        //cy.get('#checkboxes > :nth-child(1) > input').check()
+        //cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
+
+        // This checks the boxes and validates by using alias 
+        cy.get('#checkboxes > :nth-child(1) > input').as('option-1')
+        //cy.get('@option-1').check();
+        cy.get('@option-1').check().should('be.checked')
+    });
+
+    it("Uncheck and validate the checkbox to see when it is not checked", () => {
+        cy.visit("https://www.webdriveruniversity.com")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+
+        cy.xpath("//input[@value='option-3']").as('option-3')
+        // Not safe to chain commands with uncheck or check 
+        cy.get('@option-3').uncheck()
+        cy.get('@option-3').should('be.not.checked')
+
+    });
+
+    it.only("check all of the boxes", () => {
+        cy.visit("https://www.webdriveruniversity.com")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+
+        cy.xpath("//input[@value='option-3']").as('option-3')
+        // Not safe to chain commands with uncheck or check 
+        cy.get('@option-3').uncheck()
+        cy.get('@option-3').should('be.not.checked')
+
+        // checks all of the box automatically. Specific checks require there be a value in the paratheses like 'option-1
+        cy.get("input[type='checkbox']").check().should('be.checked');
+
+
+
+    });
+})
