8daa7bcb2a39ddd4ddbe6ac59d8cd50774d8acbf
Andrew Nguyen-Tran <ann013@ucsd.edu>
2024-02-05 00:22:37 -0800

    adding a debugger in the hair page.object.js

--- cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
+++ cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
@@ -2,10 +2,13 @@ class AutoStore_HairCare_Po {
     addHairCareProductsToBasket() {
         //Iterate through each of the data in the fixture/products file and passing them through the custom function
         globalThis.data.productName.forEach(function(element) {
-            cy.addProductToBasket(element)
+            cy.addProductToBasket(element).then(() => {
+
+                //debugger
+            })
         })
         // Click to see the cart contents
-        cy.get('.dropdown-toggle > .fa').click();
+        cy.get('.dropdown-toggle > .fa').click().debug();
     }
 }
 export default AutoStore_HairCare_Po;
